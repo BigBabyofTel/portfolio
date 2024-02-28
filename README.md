44f41ac55464b33a55ef8ab22b1e993d70397c4e
BigBabyofTel <augustus.tb@outlook.com>
2024-02-28 18:24:19 +0200

    nav and cards added

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## first version using Vite and React with TypeScript
 
 ### dark mode added, needs to be configured.
+
+### Dark mode delayed. Basic styling added with tailwind
--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,19 @@ const App: React.FC = () => {
     <header>
 
       <nav>
-        <ul>
-          <li>About me</li>
-          <li>Experience</li>
-          <li>Contact</li>
+        <ul className='p-5 m-5 flex justify-evenly'>
+          <li className="border p-4 shadow-2xl rounded-full bg-cyan-700 text-white">About me</li>
+          <li className="border p-4 shadow-2xl rounded-full bg-cyan-700 text-white">Experience</li>
+          <li className="border p-4 shadow-2xl rounded-full bg-cyan-700 text-white">Contact</li>
         </ul>
       </nav>
     </header>
-    <main className="text-center w-1/2 border-4 m-2">
-    <h1>T. Augustus Baker</h1>
-    <p>By day, I'm an English teacher for stage 5. I've worked in the American system; however, most of my experience is in the British system using Cambridge International Examination resources.</p>
-    <p>I've enjoyed teaching and cultivating a bond with my students that makes learning fun and enjoyable. With all my strength, I think of fun and innovative ways to deliver the lesson everyday. </p>
+    <main className="flex justify-center">
+    <section className="text-center w-1/2 border border-blue-800 m-2 p-5 shadow-2xl shadow-blue-950 bg-cyan-700">
+    <h1 className="p-4">T. Augustus Baker</h1>
+    <p>By day, I'm an <span>English teacher</span> for stage 5. I've worked in the American system; however, most of my experience is in the British system using Cambridge International Examination resources.</p>
+    <p className='p-4'>I've enjoyed teaching and cultivating a bond with my students that makes learning fun and enjoyable. With all my strength, I think of fun and innovative ways to deliver the lesson everyday. </p>
+    </section>
     </main>
     </>
   )
