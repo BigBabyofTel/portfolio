b4f5ef17dde50902e5e955aeea3710253643a733
BigBabyofTel <augustus.tb@outlook.com>
2024-02-28 10:52:03 +0200

    minor styling added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import { useState } from "react";
-import DarkModeToggle from "./DarkModeToggle";
+import React from  'react';
 
 const App: React.FC = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  
 
-  //state change from darkmodetoggle
-  const handleDarkModeChange = (newIsDarkMode: boolean) => {
-    setIsDarkMode(newIsDarkMode)
-  };
-//add dark mode class conditionally
-const className = isDarkMode ? "dark" : "";
   return (
     <>
-    <div className={className && "dark"}>
-    <main className="dark:bg-white">
+    <header>
+
+      <nav>
+        <ul>
+          <li>About me</li>
+          <li>Experience</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+    <main className="text-center w-1/2 border-4 m-2">
     <h1>T. Augustus Baker</h1>
-    <p>By day, I'm an English teacher for stage 5.</p>
-    <DarkModeToggle onChange={handleDarkModeChange} />
+    <p>By day, I'm an English teacher for stage 5. I've worked in the American system; however, most of my experience is in the British system using Cambridge International Examination resources.</p>
+    <p>I've enjoyed teaching and cultivating a bond with my students that makes learning fun and enjoyable. With all my strength, I think of fun and innovative ways to deliver the lesson everyday. </p>
     </main>
-    </div>
     </>
   )
 }
